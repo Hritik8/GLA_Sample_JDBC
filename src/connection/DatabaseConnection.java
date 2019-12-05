4472eaffbb7d4942f084d43d85a096b1f01a6d62
dbc2201 <divyanshb22@gmail.com>
2019-12-05 23:59:51 +0530

    class:DatabaseConnection; create static field for object of class:java.sql.Connection

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -7,8 +7,11 @@
 
 package connection;
 
+import java.sql.Connection;
+
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/GLA";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
+    private static Connection connection = null;
 }
