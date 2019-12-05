6934cf2f68bfd1178dcba8ef24bf37ad717b1fd6
dbc2201 <divyanshb22@gmail.com>
2019-12-06 00:45:33 +0530

    class:StudentEntryHelper; method:getNameFromUser(); return the entered value

--- src/databaseHelper/StudentEntryHelper.java
+++ src/databaseHelper/StudentEntryHelper.java
@@ -41,7 +41,7 @@ public class StudentEntryHelper {
 
     public String getNameFromUser() {
         System.out.println("Enter the name of the student: ");
-        return "";
+        return scanner.nextLine().trim();
     }
 
 }
