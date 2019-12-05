70fcc28cdaf5457c2a2e00ad178d2b1bbb916855
dbc2201 <divyanshb22@gmail.com>
2019-12-06 00:00:20 +0530

    class:DatabaseConnection; create getter method for object of class:java.sql.Connection

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -14,4 +14,8 @@ public class DatabaseConnection {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     private static Connection connection = null;
+
+    public static Connection getConnection() {
+        return connection;
+    }
 }
