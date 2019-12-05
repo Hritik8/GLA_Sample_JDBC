2a0324bbf251b8986a99766b91bd60c73580679b
dbc2201 <divyanshb22@gmail.com>
2019-12-05 23:59:07 +0530

    class:DatabaseConnection; create static field for USERNAME

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -9,4 +9,5 @@ package connection;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/GLA";
+    private static final String USERNAME = "root";
 }
