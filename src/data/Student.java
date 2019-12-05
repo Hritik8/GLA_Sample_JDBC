55b271266fc58da8d53020d26557d8184f95547c
dbc2201 <divyanshb22@gmail.com>
2019-12-06 00:07:48 +0530

    class:Student; add getters and setters for the class

--- src/data/Student.java
+++ src/data/Student.java
@@ -13,6 +13,38 @@ public class Student {
     private int year;
     private double cpi;
 
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getCpi() {
+        return cpi;
+    }
+
+    public void setCpi(double cpi) {
+        this.cpi = cpi;
+    }
+
     public Student(int rollNumber, String name, int year, double cpi) {
         this.rollNumber = rollNumber;
         this.name = name;
