97f29f7877ec723fc16b123e3bc27c79c4737eb3
dbc2201 <divyanshb22@gmail.com>
2019-12-06 01:06:08 +0530

    class:Main; reformat and add commits

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,15 +15,27 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+
+        // create the connection to the database
         DatabaseConnection connection = new DatabaseConnection();
         connection.startConnection();
         connection.printConnectionStatus();
+
+        // create an object to the Entry Helper Class
         StudentEntryHelper entryHelper = new StudentEntryHelper();
+
+        // Input values for the Student object
         int studentRollNumber = entryHelper.getRollNumberFromUser();
         String studentName = entryHelper.getNameFromUser();
         int studentYear = entryHelper.getYearFromUser();
         double studentCurrentCPI = entryHelper.getCPIFromUser();
+
+        // Create the Student object
         Student student = new Student(studentRollNumber, studentName, studentYear, studentCurrentCPI);
+
+        // Send the student object to the database
         entryHelper.createNewStudentInDatabase(DatabaseConnection.getConnection(), student);
+        entryHelper.checkIfStudentCreated();
+
     }
 }
