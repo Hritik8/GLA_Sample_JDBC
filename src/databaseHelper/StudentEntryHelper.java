3fdecfb425802afbf11b41ead37c3e4b4f05596b
dbc2201 <divyanshb22@gmail.com>
2019-12-06 00:11:57 +0530

    class: StudentEntryHelper; add JavaDoc comment for the method:addNewStudentInDatabase

--- src/databaseHelper/StudentEntryHelper.java
+++ src/databaseHelper/StudentEntryHelper.java
@@ -16,6 +16,10 @@ public class StudentEntryHelper {
      * We are going to include the CRUD operations as methods in this class.
      * */
 
+
+    /**
+     * This method will help us to store the values of a student to the database
+     */
     public boolean createNewStudentInDatabase(Connection connection, Student student) {
         return false;
     }
