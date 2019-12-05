749cbe33523b58076f7c59cef860d5a874005d0a
dbc2201 <divyanshb22@gmail.com>
2019-12-06 00:11:15 +0530

    class: StudentEntryHelper; create method:addNewStudentInDatabase with params:java.lang.Connection connection and Student student

--- src/databaseHelper/StudentEntryHelper.java
+++ src/databaseHelper/StudentEntryHelper.java
@@ -7,8 +7,17 @@
 
 package databaseHelper;
 
+import data.Student;
+
+import java.sql.Connection;
+
 public class StudentEntryHelper {
     /*
      * We are going to include the CRUD operations as methods in this class.
      * */
+
+    public boolean createNewStudentInDatabase(Connection connection, Student student) {
+        return false;
+    }
+
 }
